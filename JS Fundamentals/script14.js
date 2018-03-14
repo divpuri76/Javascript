53ca81472921844ed61d033a29bf109f378f05b8
Div Puri <divpuri@me.com>
2018-03-14 15:35:57 +0000

    Added Javascript files

---
+++ JS Fundamentals/script14.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // start up code goes here
+   // alert("this works!");
+    // $('#title').text("Yay, I can get to the H1!");
+    // $('#first').html("<h2>Great quotes</h2>");
+    
+    //append and prepend work INSIDE the given selection
+   // $('#first').prepend("<h2>Great quotes</h2>");
+    // $('#first').append("<h3>...for you to ponder...</h3>");
+    
+    //before, after, insertBefore, insertAfter work OUTSIDE
+    //the given selection. Research it!!.
+    // $("#title").addClass("standout");
+    $('#first').fadeOut().fadeIn();
+});
